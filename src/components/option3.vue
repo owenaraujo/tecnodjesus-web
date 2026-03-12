6e79d6cd6b4b794a44f7bcad396e78ab8eb45178
Owen araujo <31048440+owenaraujo@users.noreply.github.com>
2026-03-11 19:05:34 -0500

    arreglo de descripcion a 10px

--- src/components/option3.vue
+++ src/components/option3.vue
@@ -111,7 +111,7 @@
             >
           </div>
           <h3>{{ product.name }}</h3>
-          <a style="font-size: 5px;"> {{product.description}}</a>
+          <a style="font-size: 10px;"> {{product.description}}</a>
           <p class="price" v-if="product.price">${{ product.price.toLocaleString() }}</p>
           <a :href="`https://wa.me/573505456465?text=${encodeURIComponent('Hola, me interesa: ' + product.name)}`" target="_blank" class="whatsapp-btn">Consultar</a>
         </div>
